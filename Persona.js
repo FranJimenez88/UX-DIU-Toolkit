2bd4214dfb2924928ce6f04588e6e524bd1ac697
FranJimenez88 <74068401+FranJimenez88@users.noreply.github.com>
2021-03-10 19:00:05 +0100

    Actualizar personajes

--- Persona.js
+++ Persona.js
@@ -17,9 +17,9 @@
 
 angular.module("angular", [])
 	.controller("controller", ["$scope", function($scope) { 
-        $scope.Grupo_ID ="DIU1.ABCDEF";
+        $scope.Grupo_ID ="DIU3.FLR";
         $scope.Curso ="2020/21";
-        $scope.Github_ID ="https://github.com/mgea/UX-DIU-Toolkit";
+        $scope.Github_ID ="https://github.com/FranJimenez88/UX-DIU-Toolkit";
         
 		$scope.PersonaIndex = 0;
 		$scope.Personas = [
@@ -33,36 +33,36 @@ angular.module("angular", [])
                 
                 
 				Id: 0,
-				Name: "Pedro",
-				Photo: "man.png",
-				Quote: "Carpe Diem",
-				Age: 32,
-				Occupation: "Comercial en empresa de automocion",
-				Family: "Con pareja desde hace 1 año",
-				Location: "Granada (Huetor Vega)",
-				Character: "Le gusta disfrutar del tiempo",
+				Name: "José-Pedro Villanueva",
+				Photo: "jose.jpg",
+				Quote: "Never surrender",
+				Age: 45,
+				Occupation: "Profesor de educación primaria",
+				Family: "Casado desde hace 15 años, dos hijos uno de 12 y otro de 15 años",
+				Location: "A Coruña(Monte Alto)",
+				Character: "Le gusta pasar tiempo con su familia y enseñar a sus alumnos",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 1 }
 				], 
-				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
-				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
-				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
+				Goals: ["Viajar a Japón con su familia", "Ser director del colegio"],
+				Frustrations: ["Es el único que trabaja en la familia, pero tiene bastante dinero ahorrado", "Le gustaría que cambiase el sistema educativo del país"],
+				Bio: "Es de Murcia pero vive en Monte Alto(A Coruña) ya que es donde le asignaron cuando hizo las oposiciones y ahí conocio a su esposa, como le gustaba la ciudad decidio quedarse ahí, actualmente lleva trabajando en el colegio de sus propios hijos durante 10 años, ha establecido muchas amistades y es muy querido por los de su barrio",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 2 },
-					{ Name: "Movil", Value: 2 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 2 }
+					{ Name: "Movil", Value: 3 },
+					{ Name: "RRSS", Value: 2 },
+					{ Name: "Software", Value: 1 }
 					
 				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+                Contextos: "Su sueño siempre ha sido viajar a Japón y como lleva un tiempo sin unas vacaciones por la pandemia y quiere darle una sorpresa a la familia con el viaje a Japón",  
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional", Value: 5 },
-					{ Name: "Online & Social Media", Value: 3 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+					{ Name: "Publicidad Tradicional", Value: 4 },
+					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
 				]
 			},
 			{	
@@ -74,36 +74,36 @@ angular.module("angular", [])
                 
                 
 				Id: 1,
-				Name: "Monica Suarez",
-				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Name: "Sara Gil",
+				Photo: "sara.jpg",
+				Quote: "Tempus fugit",
+				Age: 21,
+				Occupation: "Estudiante de Enfermería",
+				Family: "Padre y madre, y una hermana pequeña",
+				Location: "Málaga",
+				Character: "Pensamiento crítico e insegura",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 1 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 4 }
 				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Goals: ["Aprobar las oposiciones y encontrar trabajo", "Dejar de ser insegura", "Independizarse", "Encontrar el amor de su vida"],
+				Frustrations: ["Le gustaría tener mas concentración para poder sacar las oposiciones a la primera ", "Le da miedo que su futuro trabajo no le llene tanto"],
+				Bio: "Reside desde su nacimiento en Málaga, siempre ha sido una buena estudiante y una persona sin maldad, pero problemas de bullying en primaria le ha hecho más insegura aunque actualmente a encontrado a una buenas amigas en la facultad",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
-					{ Name: "Mobile", Value: 3 },
+					{ Name: "TIC/Internet", Value: 4 },
+					{ Name: "Mobile", Value: 5 },
 					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
+					{ Name: "Software", Value: 2 }
 					
 				], 
-                Contextos:   "The goals this user hopes to achieve." ,
+                Contextos:   "Su sueño es dejar de teneer inseguridades, encontrar un trabajo que le guste hasta su jubilación y hacer un viaje con sus amigas" ,
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
-					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Publicidad Tradicional (Ads)", Value: 3 },
+					{ Name: "Online & Social Media", Value: 5 },
 					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 3 }
 				]
 			}
 		];
